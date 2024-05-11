88f0c32b2d5c6ab85a5bfab223c737c917e7b843
Shubhamssoni499 <Shubhamsoni499@gmail.com>
2024-05-11 19:29:18 +0530

    Add_product

---
+++ django-task/app/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("",index),
+    path("ragistration/", create_user),
+    path("table/", table),
+    path("delete/<int:pk>/",delete_user,name="delete"),
+    path("update/<int:uid>/",update,name="update"),
+    path("update_user/",update_data),
+    # path("login/",login_view),
+    path("login_us/",login),
+    path("login/",login_user),
+    path("product/",product),
+    path("login/",login_user),
+    path("add_product/",add_product)
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) ##FOR IMAGE UPLOAD
+
